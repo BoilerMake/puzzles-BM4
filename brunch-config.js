29fa2fb68721aa37175f438d907d37cd46299e09
samontea <merciers.merciers@gmail.com>
2016-08-02 15:51:04 -0400

    added babel for transpilation

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,11 @@
 module.exports = {
   // See http://brunch.io for documentation.
+  plugins: {
+    babel: {
+      presets: ['es2015', 'es2016'],
+      pattern: /\.(es6|js)$/
+    }
+  },
   files: {
       javascripts: {
 		  joinTo: {
