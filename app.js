462a98bb185b8cf437299bec31a17820a5dd1f0c
samontea <merciers.merciers@gmail.com>
2016-08-04 21:25:45 -0400

    added puzzle 3

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const brunch = spawn('brunch', ['watch'], {
 	stdio: 'inherit'
 });
 
-var express = require('express');
+const express = require('express');
 var app = express();
 app.use(express.static('public'));
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.post(
@@ -29,6 +29,22 @@ app.post(
 	}
 );
 
+app.post(
+	'/puzzle3',
+	(req, res) => {
+		res.status(200).send({spooked: 'boo'});
+	}
+
+app.get(
+	'/donttalktomygoatorhiskideveragain',
+	(req, res) => {
+		// TODO if they're on puzzle 3
+		console.log('passed');
+		res.redirect('/');
+		// TODO else res.sendStatus(404);
+	}
+);
+
 app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+	console.log('Puzzles listening on port 3000!');
 });
