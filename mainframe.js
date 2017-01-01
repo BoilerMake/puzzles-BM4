59223211698de0fb6fe9563cd3fdd5acff79db33
samontea <merciers.merciers@gmail.com>
2017-01-01 11:45:17 -0600

    removed unnecessary variables

--- mainframe.js
+++ mainframe.js
@@ -2,33 +2,30 @@ var request = require('superagent')
 var jwtDecode = require('jwt-decode')
 require('dotenv').config()
 
-const MainFrame = {
-  parseAuthorization: (authorization) => {
-    if('Bearer ' === authorization.substring(0,7))
-      return authorization.substring(7)
-    else
-      return authorization
-  },
+const parseAuthorization = (authorization) => {
+  if('Bearer ' === authorization.substring(0,7))
+    return authorization.substring(7)
+  else
+    return authorization
+}
 
+const MainFrame = {
   getUserId: (authorization) => {
-    const jwtToken = parseAuthorization(authorization)
-    return jwtDecode(jwtToken).user_id || 0
+    return jwtDecode(parseAuthorization(authorization)).user_id || 0
   },
 
   completePuzzle: (authorization, puzzleNumber) => {
-    const jwtToken = parseAuthorization(authorization)
     request
-      .post(`${process.env.API_ENDPOINT}/v1/users/me/puzzles?puzzle_id=${puzzleNumber}&puzzle_secret=${process.env.PUZZLE_SECRET}&token=${jwtToken}`).then(
+      .post(`${process.env.API_ENDPOINT}/v1/users/me/puzzles?puzzle_id=${puzzleNumber}&puzzle_secret=${process.env.PUZZLE_SECRET}&token=${parseAuthorization(authorization)}`).then(
         success => {},
         fail => {console.log(fail)}
       )
   },
 
   getCompletedPuzzles: (authorization) => {
-    const jwtToken = parseAuthorization(authorization)
     return request
-      .get(`${process.env.API_ENDPOINT}/v1/users/me/puzzles?token=${jwtToken}`)
-      .then( res => {
+      .get(`${process.env.API_ENDPOINT}/v1/users/me/puzzles?token=${parseAuthorization(authorization)}`)
+      .then(res => {
         return res.body.puzzles
       }, reason => {
         console.log(reason)
